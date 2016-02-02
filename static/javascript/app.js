87cf3198925363f74f22d7fd6a23f212be1079b1
Shanfan <chitchy@gmail.com>
2016-02-01 23:49:37 -0500

    update JSON reference to use dot syntax

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,8 +1,17 @@
 function drawBus(bus, map){
   return {
-    position: {lat: bus['lat'], lng: bus['lon']},
+    position: {lat: bus.lat, lng: bus.lon},
     map: map,
     icon: "/static/bus.png",
-    title: "Route: " + bus['route'] + "-" + "Bearing:" + bus['bearing']
+    title: "Route: " + bus.route + "-" + "Bearing:" + bus.bearing
   };
+}
+
+function drawStop(stop, map){
+  return {
+    position: {lat: stop.stop_lat, lng: stop.stop_lon},
+    map: map,
+    icon: "/static/busstop.png",
+    title: stop.stop_name + " - Routes: " + stop.routes 
+  }
 }
