8d8f16dcd3bcc87b148be397044585a076172f64
Brian Sanders <bdougsand+cfb@gmail.com>
2016-02-12 22:27:57 +0000

    Draw little buses on the map

--- static/javascript/start.js
+++ static/javascript/start.js
@@ -1,38 +1,38 @@
-define(["jquery", "leaflet"], function($, L) {
-    return {
-        init: function() {
-            var map = L.map("map", {
-                center: [42.36564700281194, -71.06386184692381],
-                zoom: 15
-            });
-            L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png")
-                .addTo(map);
+define(["jquery", "leaflet", "bus-marker"],
+       function($, L, BusMarker) {
+           return {
+               init: function() {
+                   var map = L.map("map", {
+                       center: [42.36564700281194, -71.06386184692381],
+                       zoom: 15
+                   });
+                   L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png")
+                       .addTo(map);
 
-            var lastTick = 0,
-                busMarkers = {},
-                busLayer = L.layerGroup().addTo(map);
-            function tick(){
-                $.get("/api/bus_updates", {route: "77",
-                                           stamp: lastTick}).
-                    then(function(update) {
-                        console.log(update, update.buses);
-                        $.each(update.buses, function(idx, bus) {
-                            if (!busMarkers[bus.id]) {
-                                busMarkers[bus.id] =
-                                    L.marker([+bus.lat, +bus.lon])
-                                    .addTo(busLayer);
-                            } else {
-                                busMarkers[bus.id].setLatLng([+bus.lat, +bus.lon]);
-                            }
-                        });
+                   var lastTick = 0,
+                       busMarkers = {},
+                       busLayer = L.layerGroup().addTo(map);
+                   function tick(){
+                       $.get("/api/bus_updates", {route: "77",
+                                                  stamp: lastTick}).
+                           then(function(update) {
+                               $.each(update.buses, function(idx, bus) {
+                                   if (!busMarkers[bus.id]) {
+                                       busMarkers[bus.id] =
+                                           new BusMarker(bus)
+                                           .addTo(busLayer);
+                                   } else {
+                                       busMarkers[bus.id].update(bus);
+                                   }
+                               });
 
-                        setTimeout(tick, 10000);
-                    });
-            }
+                               setTimeout(tick, 10000);
+                           });
+                   }
 
-            tick();
+                   tick();
 
-            window.busMarkers = busMarkers;
-        }
-    };
-});
+                   window.busMarkers = busMarkers;
+               }
+           };
+       });
