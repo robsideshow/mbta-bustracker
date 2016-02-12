36d9dc9ae21b2fdee6e116d524974623402cd44d
robsideshow <robshowsides@gmail.com>
2016-02-11 21:11:22 -0500

    Added endquote which must have been lost in the matirx

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,7 +1,7 @@
 require.config({
     paths: {
         "jquery": "http://code.jquery.com/jquery-1.12.0.min",
-        "leaflet": "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet,
+        "leaflet": "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet",
 
         "start": "start"
     },
