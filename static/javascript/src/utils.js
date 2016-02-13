4592248ce359c590841aa9b560719a33a6483fa9
Brian Sanders <bdougsand+cfb@gmail.com>
2016-02-13 23:13:30 +0000

    Convert heading to compass direction

---
+++ static/javascript/src/utils.js
@@ -0,0 +1,18 @@
+define([], function() {
+    return {
+        directions: ["north", "northeast", "east", "southeast",
+                     "south", "southwest", "west", "northwest",
+                     "north"],
+        directionsShort: ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"],
+        
+        readableHeading: function(heading) {
+            return this.directions[Math.round(heading%360/45)];
+        },
+
+        readableHeadingShort: function(heading) {
+            return this.directionsShort[Math.round(heading%360/45)];
+        },
+
+        makeTransformFn: null
+    };
+});
