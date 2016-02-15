be3d748faca454c81ab7df9ce7c6b8a96f62523f
Brian Sanders <bdougsand+cfb@gmail.com>
2016-02-15 13:50:56 +0000

    Dumb little arrows to indicate vehicle heading

--- static/javascript/src/busMarker.js
+++ static/javascript/src/busMarker.js
@@ -32,15 +32,20 @@ define(["leaflet", "utils"],
                onAdd: function(map) {
                    this._map = map;
 
-                   this.busShape = L.polygon(this._calculatePoints(),
+                   this.busShape = L.polygon(this._busPoints(),
                                              {
                                                  color: "#000",
                                                  weight: 2,
                                                  fill: true,
-                                                 fillColor: "white"
+                                                 fillColor: "white",
+                                                 fillOpacity: 0.9
+
                                              })
                        .addTo(this);
-                   this.centerPoint = L.circle(this.getCenter(), 2).addTo(this);
+                   this.arrowShape =
+                       L.polyline(this._arrowPoints(), {color: "#ff0000",
+                                                        weight: 3})
+                        .addTo(this);
                },
 
                onPopupOpen: function(e) {
@@ -51,36 +56,71 @@ define(["leaflet", "utils"],
                    return L.latLng(+this.bus.lat, +this.bus.lon);
                },
 
-               _calculatePoints: function() {
+               /**
+                * Create and return a function for transforming a point from the
+                * coordinate system of the bus to latitude and longitude.
+                *
+                * @param {L.CRS} [crs=L.CRS.EPSG3857]
+                * @return {Function} A function that takes an array [x, y] and
+                * returns a L.LatLng instance.
+                */
+               makeTransform: function(crs) {
+                   crs = crs || L.CRS.EPSG3857;
+                   var cached = this._cachedTransform;
+                   if (cached && cached.crs == crs.code &&
+                       cached.heading == this.bus.heading) {
+                       return cached.transform;
+                   }
+
                    var bus = this.bus,
                        degs = (360-(bus.heading-90))%360,
                        rads = degs/180 * Math.PI,
-                       crs = L.CRS.EPSG3857,
                        busLatLng = L.latLng(+bus.lat, +bus.lon),
                        centerPoint = crs.latLngToPoint(busLatLng, 15),
                        // cached valued for calculation:
                        cx = centerPoint.x,
                        cy = centerPoint.y,
-                       hl = this.options.busLength/2,
-                       hw = this.options.busWidth/2,
-                       polyPoints = [[-hl, hw], [hl, hw], [hl, -hw], [-hl, -hw]],
                        sinRads = Math.sin(-rads),
                        cosRads = Math.cos(-rads);
 
-                   return polyPoints.map(function(point) {
+                   var transform = function(point) {
                        var x = point[0] * cosRads - point[1] * sinRads,
                            y = point[0] * sinRads + point[1] * cosRads;
 
                        return crs.pointToLatLng(L.point(cx+x, cy+y), 15);
-                   });
+                   };
+
+                   this._cachedTransform = {
+                       crs: crs.code,
+                       transform: transform,
+                       heading: bus.heading
+                   };
+
+                   return transform;
+               },
+
+               _busPoints: function() {
+                   var transform = this.makeTransform(),
+                       hl = this.options.busLength/2,
+                       hw = this.options.busWidth/2,
+                       polyPoints = [[-hl, hw], [hl, hw],
+                                     [hl, -hw], [-hl, -hw]];
+
+                   return polyPoints.map(transform);
+               },
+
+               _arrowPoints: function() {
+                   var transform = this.makeTransform(),
+                       points = [[5, 0], [10, 0], [8, 2], [10, 0], [8, -2]];
+
+                   return points.map(transform);
                },
 
                update: function(bus) {
                    this.bus = bus;
 
-                   var points = this._calculatePoints();
-                   this.busShape.setLatLngs(points);
-                   this.centerPoint.setLatLng(this.getCenter());
+                   this.busShape.setLatLngs(this._busPoints());
+                   this.arrowShape.setLatLngs(this._arrowPoints());
                }
            });
        });
