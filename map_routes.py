05743831066b112430ce0f06bbdd73a63ed6030d
Shanfan Huang <shuang@pivotal.io>
2016-02-15 23:05:33 -0500

    Add top and side nav to liveMap for bus selection

--- map_routes.py
+++ map_routes.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, request, render_template
+import bustracker as btr
 
 map_routes = Blueprint("map", __name__)
+sortedRoute_ids, routeTitles = btr.getAllBusRoutes()
 
 @map_routes.route("/")
 def live_map():
-    return render_template("liveMap.html")
+  return render_template('liveMap.html', 
+                          routeTitles = btr.routenamesdict,
+                          rnums = sortedRoute_ids);
