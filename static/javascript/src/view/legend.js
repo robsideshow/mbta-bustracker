3a80b7bad3133edecc145a6f066bff87a7baed3c
Brian Sanders <bdougsand+cfb@gmail.com>
2016-03-16 18:54:43 +0000

    Show inbound/outbound vehicle counts in legend
    Closes #40

--- static/javascript/src/view/legend.js
+++ static/javascript/src/view/legend.js
@@ -5,32 +5,78 @@ define(["backbone"],
                    console.assert(options.app,
                                   "LegendView requires an AppState instance");
                    this.app = options.app;
-                   this.listenTo(this.app, "routeSelected", this.render)
-                       .listenTo(this.app, "routeUnselected", this.render);
+
+                   var vehicles = this.app.vehicles;
+                   this.listenTo(vehicles , "add", this.deferRender)
+                       .listenTo(vehicles, "remove", this.deferRender)
+                       .listenTo(vehicles, "change:direction", this.deferRender)
+                       .listenTo(vehicles, "change:route_id", this.deferRender);
+               },
+
+               events: {
+                   "click route-name": "onClick"
+               },
+
+               deferRender: function() {
+                   clearTimeout(this._deferred);
+
+                   var self = this;
+                   this._deferred = setTimeout(function() {
+                       self.render();
+                   }, 0);
                },
 
                render: function() {
                    var $el = this.$el,
-                       routes = this.app.getSelectedRoutes();
-
-                   $el.html("");
-
-                   if (!routes.length) {
+                       $content = $el.find("tbody");
+                   if (!this.app.route_ids.length) {
+                       $content.html("");
                        $el.hide();
                        return;
                    }
 
-                   _.each(routes, function(route) {
-                       $el.append(
-                           "<div class='route'>" +
+                   var routes = this.app.routes,
+                       routeInfo = _.reduce(this.app.route_ids,
+                                            function(m, route_id) {
+                                                m[route_id] = {inbound: 0,
+                                                               outbound: 0};
+                                                return m;
+                                            }, {});
+
+                   this.app.vehicles.each(
+                       function(vehicle) {
+                           var route_id = vehicle.get("route_id");
+                           try {
+                               routeInfo[route_id][vehicle.get("direction") == "1" ?
+                                                   "inbound" : "outbound"] += 1;
+                           } catch (err) {
+                               // Only count vehicles that have route_id values
+                               // in the route_ids array.
+                           }
+                       });
+
+                   $content.html("");
+
+                   _.each(routeInfo, function(info, route_id) {
+                       var route = routes.get(route_id);
+                       $content.append(
+                           "<tr class='route'>" +
+                               "<td class='route-name'>" + 
                                "<div class='swatch' style='background-color:" +
                                route.getColor() + "'>" +
                                "</div> " + _.escape(route.getName()) +
-                            "</div>"
+                               "</td>" +
+                               "<td>" + info.inbound + "</td>" +
+                               "<td>" + info.outbound + "</td>" +
+                               "</tr>"
                        );
                    });
 
                    $el.show();
+               },
+
+               onClick: function() {
+                   // TODO: Refocus the map on the clicked legend
                }
            });
 
