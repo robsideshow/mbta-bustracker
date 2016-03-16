9d26dc0ea37916ca45b3ad8514832f97d17c6787
Brian Sanders <bdougsand+cfb@gmail.com>
2016-03-16 18:56:03 +0000

    Shape#getBounds() method

--- static/javascript/src/model/shape.js
+++ static/javascript/src/model/shape.js
@@ -1,4 +1,13 @@
-define(["backbone"],
-       function(B) {
-           return B.Model;
+define(["backbone", "leaflet"],
+       function(B, L) {
+           var ShapeModel = B.Model.extend({
+               getBounds: function() {
+                   if (!this._cachedBounds)
+                       this._cachedBounds = L.latLngBounds(this.get("path"));
+
+                   return this._cachedBounds;
+               }
+           });
+
+           return ShapeModel;
        });
