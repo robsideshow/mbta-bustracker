11955bec061ed44972b2db85090c64b508132881
Brian Sanders <bdougsand+cfb@gmail.com>
2016-03-17 20:04:20 +0000

    Made StopModel and StopCollection aware of parents

--- static/javascript/src/collection/stops.js
+++ static/javascript/src/collection/stops.js
@@ -1,7 +1,44 @@
 define(["backbone", "stop-model"],
        function(B, StopModel) {
            var StopsCollection = B.Collection.extend({
-               model: StopModel
+               model: StopModel,
+
+               initialize: function() {
+                   B.Collection.prototype.initialize.apply(this, arguments);
+                   this.listenTo(this, "add", this.onAdd)
+                       .listenTo(this, "remove", this.onRemove);
+               },
+
+               onAdd: function(stop) {
+                   var parent_id = stop.get("parent");
+
+                   if (!parent_id) return;
+
+                   var parent = this.get(parent_id);
+
+                   if (!parent) {
+                       parent = new StopModel({stop_id: parent_id,
+                                               lat: stop.get("lat"),
+                                               lon: stop.get("lon"),
+                                               isParent: true});
+                       parent.children = {};
+                       this.add(parent);
+                   }
+
+                   parent.children[stop.id] = stop;
+               },
+
+               onRemove: function(stop) {
+                   var parent = stop.getParent();
+
+                   if (parent) {
+                       delete parent.children[stop.id];
+                       if (_.isEmpty(parent.children)) {
+                           delete parent.children;
+                           this.remove(parent);
+                       }
+                   }
+               }
            });
 
            return StopsCollection;
--- static/javascript/src/model/stop.js
+++ static/javascript/src/model/stop.js
@@ -1,10 +1,37 @@
-define(["backbone", "leaflet"],
-       function(B, L) {
+define(["backbone", "leaflet", "underscore"],
+       function(B, L, _) {
            var StopModel = B.Model.extend({
                idAttribute: "stop_id",
 
                getLatLng: function() {
                    return L.latLng(this.get("lat"), this.get("lon"));
+               },
+
+               getName: function() {
+                   return this.get("stop_name");
+               },
+
+               getChildren: function() {
+                   if (this.children)
+                       return _.values(this.children);
+
+                   return [];
+               },
+
+               getSiblings: function() {
+                   var parent = this.getParent();
+                   if (parent)
+                       return _.values(parent.children);
+
+                   return [];
+               },
+
+               getParent: function() {
+                   return this.collection.get(this.get("parent"));
+               },
+
+               isParent: function() {
+                   return !!this.get("isParent");
                }
            });
 
