29e7c6b29f5248d0d0a2a0c27e891b6ae20e5c39
Shanfan <chitchy@gmail.com>
2016-04-05 00:17:54 -0400

    Refactor the UI - WIP

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,12 +1,33 @@
 /* Global UI Behavior should come here */
 
 define(["jquery"], function($){
+    $(document).ready(function(){
+        $("graph-view").hide("fast");
+
+        var hideCtrl = false;
+        $(".routeCtrl").on("click",function(){
+            console.log(hideCtrl);
+
+            if (!hideCtrl) {
+                hideCtrl = true;
+                $(this).html('<i class="material-icons">vertical_align_bottom</i> Pullout Route Control');
+                console.log(hideCtrl);
+
+                $(".routeToggleControl").slideup();
+            }else{
+                hideCtrl = false;
+                $(this).html('<i class="material-icons">vertical_align_top</i> Hide Route Control');
+                console.log(hideCtrl);
+
+                $(".routeToggleControl").slidedown();
+            }
+        });
+    })
     $("#map-view").click(function(){
         $(this).parent().addClass("active");
         $("#graph-view").parent().removeClass("active");
         $("#map").fadeIn("fast");
         $("#graph").fadeOut("fast");
-        console.log("map-view is clicked!");
     });
 
     $("#graph-view").click(function(){
@@ -15,4 +36,6 @@ define(["jquery"], function($){
         $("#map").fadeOut("fast");
         $("#graph").fadeIn("fast");
     });
+
+
 });
