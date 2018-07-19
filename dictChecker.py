a68032267f8518b375e0e0aa9d5ce256857c2b47
Brian Sanders <bdougsand+cfb@gmail.com>
2018-07-19 18:11:25 -0400

    WIP: Removed dependence on server's feed_info.txt file
    Provisional rewrite of dictChecker.getJson() that uses the If-Modified-Since and
    Last-Modified headers instead of comparing the contents of the `feed_info.txt`
    file, which is deprecated.

--- dictChecker.py
+++ dictChecker.py
@@ -19,6 +19,7 @@ This module contains functions that are used to:
 
 import requests, zipfile, os, dictmaker, shutil, logging, sys
 from io import BytesIO
+from datetime import datetime
 
 logging.basicConfig(filename='ignore/bustracker.log',level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -26,50 +27,18 @@ logger = logging.getLogger(__name__)
 
 info_url = 'http://www.mbta.com/uploadedfiles/feed_info.txt'
 
-gtfs_zip_url = 'http://www.mbta.com/uploadedfiles/MBTA_GTFS.zip'
+gtfs_zip_url = 'https://cdn.mbta.com/MBTA_GTFS.zip'
 
 class ZipDownloadException(Exception):
    pass
 
-def getMBTAGtfsInfo():
-    '''
-    get the version info from the MBTA url
-    '''
+
+def download_response_body(response, filename):
     try:
-        r = requests.get(info_url)
-        if r.ok:
-            info = (r.content).split('",')[-1].strip()
-            version = info.strip('"')
-        return version
-    except:
-        er = sys.exc_info()
-        logger.error(er)
-        return 'trouble getting MBTA version info'
-
-def getLocalGtfsInfo():
-    '''
-    get the GTFS version info from the local disk
-    '''
-    if os.path.exists('MBTA_GTFS_texts/feed_info.txt'):
-        try:
-            f = open('MBTA_GTFS_texts/feed_info.txt', 'r')
-            info = f.read().split('",')[-1].strip()
-            version = info.strip('"')
-            return version
-        except:
-            er = sys.exc_info()
-            logger.error(er)
-    return 'trouble getting local info file'
-
-
-def getZipFile():
-    print 'downloading and extracting static GTFS text files from MBTA...'
-    try:
-        r = requests.get(gtfs_zip_url, stream=True)
-        byte_length = float(r.headers['content-length'])
+        byte_length = float(response.headers['content-length'])
         perc_notify = 10
         byte_buffer = BytesIO()
-        for chunk in r.iter_content(chunk_size=1024):
+        for chunk in response.iter_content(chunk_size=1024):
             byte_buffer.write(chunk)
             percent = byte_buffer.tell()/byte_length*100
             if percent >= perc_notify:
@@ -77,32 +46,38 @@ def getZipFile():
                 perc_notify += 10
         logger.info("Download complete.")
         z = zipfile.ZipFile(byte_buffer)
-        z.extractall('MBTA_GTFS_texts')
+        z.extractall(filename)
     except:
        er = sys.exc_info()
-       logger.error(er)
+       logger.exception("Error while downloading response")
        raise
 
+
 def updateJson():
-    '''
-    1) checks whether the \MBTA_GTFS_texts folder exists
+   headers = {}
+   try:
+      with open("cache/last_modified.txt") as lmfile:
+         headers["If-Modified-Since"] = lmfile.read().strip()
+   except IOError:
+      headers["If-Modified-Since"] = None
 
-    2) if so, checks whether the version of the feed_info.txt matches the
-    current version at MBTA website.  If not, download new GTFS text files and
-    create new json files in \data folder.
+   response = requests.get(gtfs_zip_url, headers=headers, stream=True)
 
-    3) even if local MBTA file version matches current version at MBTA website, still
-    check to make sure \data folder exists.  If not, create it and json files.
+   if response.status_code == 304:
+      return
 
-    '''
-    if not (os.path.exists('MBTA_GTFS_texts/feed_info.txt') and
-                    getLocalGtfsInfo() == getMBTAGtfsInfo()):
-        getZipFile()
-        if os.path.exists('data'):
-            shutil.rmtree('data')
-        os.mkdir('data')
-        dictmaker.makeAllDicts()
+   if response.status_code != 200:
+      # Report the error (or whatever)
+      pass
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
-        dictmaker.makeAllDicts()
+   try:
+      os.mkdir("cache")
+   except:
+      pass
+
+   download_response_body(response, "MBTA_GTFS_texts")
+   with open("cache/last_modified.txt", "w") as lmfile:
+      lmfile.write(response.headers["Last-Modified"])
+
+   shutil.rmtree("data")
+   dictmaker.makeAllDicts()
